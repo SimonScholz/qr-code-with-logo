7a86ba377ec3d7f26e92683f59715830ebcdf2a4
Simon Scholz <opensource.simon@gmail.com>
2023-10-03 23:07:52 +0200

    Update actions and dependencies

--- java-sample/build.gradle.kts
+++ java-sample/build.gradle.kts
@@ -11,8 +11,6 @@ repositories {
 
 dependencies {
     implementation(project(":qr-code"))
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
