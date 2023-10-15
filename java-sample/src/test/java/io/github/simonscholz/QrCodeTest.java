7a7d0c675d8cc99a6c13813e9d8de6b4dfca286c
Simon Scholz <opensource.simon@gmail.com>
2023-10-15 21:37:03 +0200

    Wrap sample test in assertDoesNotThrow

--- java-sample/src/test/java/io/github/simonscholz/QrCodeTest.java
+++ java-sample/src/test/java/io/github/simonscholz/QrCodeTest.java
@@ -8,17 +8,23 @@ import org.junit.jupiter.api.io.TempDir;
 import java.io.IOException;
 import java.nio.file.Path;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class QrCodeTest {
 
     @Test
-    public void testMain(@TempDir Path tempDir) throws IOException {
-        QrCodeApi qrCodeApi = QrCodeFactory.createQrCodeApi();
-        Main.generateSamples(qrCodeApi, tempDir.toAbsolutePath().toString());
+    public void testMain(@TempDir Path tempDir) {
+        assertDoesNotThrow(() -> {
+            QrCodeApi qrCodeApi = QrCodeFactory.createQrCodeApi();
+            Main.generateSamples(qrCodeApi, tempDir.toAbsolutePath().toString());
+        });
     }
 
     @Test
     public void testQrCodeTypesMain(@TempDir Path tempDir) throws IOException {
-        QrCodeApi qrCodeApi = QrCodeFactory.createQrCodeApi();
-        QrCodeTypesMain.generateSamples(qrCodeApi, tempDir.toAbsolutePath().toString());
+        assertDoesNotThrow(() -> {
+            QrCodeApi qrCodeApi = QrCodeFactory.createQrCodeApi();
+            QrCodeTypesMain.generateSamples(qrCodeApi, tempDir.toAbsolutePath().toString());
+        });
     }
 }
