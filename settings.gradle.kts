fce01dfc5316aa8907d3aba9ef9a60bf6fa46efc
Simon Scholz <scholz@doorbit.com>
2023-11-23 20:34:01 +0100

    Make use of a preview for jlink windows distrubutions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "./plugin-lib/repository")
+    }
+}
+
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
