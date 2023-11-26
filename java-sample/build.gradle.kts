f361732fd0c0156981147a30891a3be47b85503e
Simon Scholz <opensource.simon@gmail.com>
2023-11-26 01:31:02 +0100

    Update version to 0.3.0-SNAPSHOT

--- java-sample/build.gradle.kts
+++ java-sample/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.simonscholz:qr-code-with-logo:0.2.0-SNAPSHOT")
+    implementation("io.github.simonscholz:qr-code-with-logo:0.3.0-SNAPSHOT")
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
