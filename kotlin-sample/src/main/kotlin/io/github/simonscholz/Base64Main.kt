958df131178c0f8f329dd7cb117285c6e1c221df
Simon Scholz <scholz@doorbit.com>
2023-11-26 18:05:24 +0100

    Adjust kotlin sample with base64 enhancements

--- kotlin-sample/src/main/kotlin/io/github/simonscholz/Base64Main.kt
+++ kotlin-sample/src/main/kotlin/io/github/simonscholz/Base64Main.kt
@@ -2,6 +2,7 @@ package io.github.simonscholz
 
 import io.github.simonscholz.qrcode.QrCodeConfig
 import io.github.simonscholz.qrcode.QrCodeFactory
+import io.github.simonscholz.qrcode.toBase64
 
 /**
  * This is a sample to demonstrate the usage of the qr-code library with a base64 encoded image.
@@ -9,9 +10,12 @@ import io.github.simonscholz.qrcode.QrCodeFactory
  * It creates a qr code as a base64 encoded image and prints it to the console.
  */
 fun main() {
-    val base64QrCodeImage = QrCodeFactory.createQrCodeApi().createBase64QrCodeImage(
-        QrCodeConfig.Builder("https://simonscholz.github.io/").build(),
-    )
+    val createQrCodeApi = QrCodeFactory.createQrCodeApi()
+    QrCodeConfig.Builder("https://simonscholz.github.io/").build().run {
+        val base64QrCodeImage = createQrCodeApi.createBase64QrCodeImage(this)
+        println(base64QrCodeImage)
 
-    println(base64QrCodeImage)
+        val toBase64 = createQrCodeApi.createQrCodeImage(this).toBase64()
+        println(toBase64)
+    }
 }
