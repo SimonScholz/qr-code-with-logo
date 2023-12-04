9c4fb682d6095d51b9718d049df6286d1672b448
Simon Scholz <scholz@doorbit.com>
2023-12-05 00:41:54 +0100

    Add the shadow plugin for fat jars

--- qr-code-app/build.gradle.kts
+++ qr-code-app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 
     id("org.graalvm.buildtools.native") version "0.9.28"
     id("com.ryandens.jlink-application") version "0.4.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
