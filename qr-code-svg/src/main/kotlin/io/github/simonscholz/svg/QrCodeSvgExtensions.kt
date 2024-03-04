10067b4b35f354d5139064e21196b529cd5609c3
Simon Scholz <opensource.simon@gmail.com>
2024-03-04 20:40:48 +0100

    Restructure Code and make impl internal

---
+++ qr-code-svg/src/main/kotlin/io/github/simonscholz/svg/QrCodeSvgExtensions.kt
@@ -0,0 +1,40 @@
+package io.github.simonscholz.svg
+
+import org.apache.batik.svggen.SVGGraphics2D
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
+
+fun Document.getSVGDimensions(): Pair<Double, Double>? {
+    val svgElements: NodeList = this.getElementsByTagName("svg")
+    if (svgElements.length > 0) {
+        val svgElement = svgElements.item(0) as Element
+        val viewBox = svgElement.getAttribute("viewBox")
+        if (viewBox.isNotBlank()) {
+            val dimensions = viewBox.split(" ")
+            if (dimensions.size == 4) {
+                val width = dimensions[2].toDouble()
+                val height = dimensions[3].toDouble()
+                return Pair(width, height)
+            }
+        }
+        // If viewBox is not specified or incorrect, try getting width and height attributes
+        val widthAttribute = svgElement.getAttribute("width").toDoubleOrNull()
+        val heightAttribute = svgElement.getAttribute("height").toDoubleOrNull()
+        if (widthAttribute != null && heightAttribute != null) {
+            return Pair(widthAttribute, heightAttribute)
+        }
+    }
+
+    return null
+}
+
+fun Document.applySvgGraphics(svgGraphics: SVGGraphics2D) {
+    val root = this.documentElement
+    val svgContent = svgGraphics.root
+    val svgChildren = svgContent.childNodes
+    for (i in 0 until svgChildren.length) {
+        val importedNode = this.importNode(svgChildren.item(i), true)
+        root.appendChild(importedNode)
+    }
+}
