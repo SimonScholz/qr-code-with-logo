03b76a8f4ed0fcde6c055c877f5f5869da9bda0b
Simon Scholz <opensource.simon@gmail.com>
2024-08-05 22:50:58 +0200

    Add .kotlin folder to gitignore

--- java-sample/src/main/java/io/github/simonscholz/SVGSamples.java
+++ java-sample/src/main/java/io/github/simonscholz/SVGSamples.java
@@ -3,7 +3,9 @@ package io.github.simonscholz;
 
 import io.github.simonscholz.svg.QrCodeSvgConfig;
 import io.github.simonscholz.svg.QrCodeSvgFactory;
+import org.w3c.dom.Document;
 
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -12,12 +14,12 @@ import java.io.File;
 
 public class SVGSamples {
     public static void main(final String[] args) throws TransformerException {
-        var qrCodeConfig = new QrCodeSvgConfig.Builder("https://simonscholz.dev/}").build();
-        var qrCodeDocument = QrCodeSvgFactory.createQrCodeApi().createQrCodeSvg(qrCodeConfig);
-        var transformerFactory = TransformerFactory.newInstance();
-        var transformer = transformerFactory.newTransformer();
-        var source = new DOMSource(qrCodeDocument);
-        var result = new StreamResult(new File("qr-code.svg"));
+        QrCodeSvgConfig qrCodeConfig = new QrCodeSvgConfig.Builder("https://simonscholz.dev/}").build();
+        Document qrCodeDocument = QrCodeSvgFactory.createQrCodeApi().createQrCodeSvg(qrCodeConfig);
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(qrCodeDocument);
+        StreamResult result = new StreamResult(new File("qr-code.svg"));
         transformer.transform(source, result);
     }
 }
