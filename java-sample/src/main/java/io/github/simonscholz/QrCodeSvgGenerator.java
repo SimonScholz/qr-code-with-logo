3cb39056b148f5eacff4a1015ff741e054c43fd8
Simon Scholz <opensource.simon@gmail.com>
2024-08-05 22:48:55 +0200

    Adjust Java code example to run with Java8

--- java-sample/src/main/java/io/github/simonscholz/QrCodeSvgGenerator.java
+++ java-sample/src/main/java/io/github/simonscholz/QrCodeSvgGenerator.java
@@ -12,8 +12,10 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.String;
 import java.util.Base64;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -25,27 +27,27 @@ import org.xml.sax.SAXException;
 public final class QrCodeSvgGenerator {
   public static void main(final String[] args) throws IOException, TransformerException,
       ParserConfigurationException, SAXException {
-    final var qrCodeGenerator = new QrCodeSvgGenerator();
-    final var logo = parseBase64EncodedStringToDocument("Replace-by-Base64-encoded-SVGImage");
-    final var qrCodeImage = qrCodeGenerator.generateQrCode(logo);
-    final var transformerFactory = TransformerFactory.newInstance();
-    final var transformer = transformerFactory.newTransformer();
-    final var source = new DOMSource(qrCodeImage);
-    final var result = new StreamResult(new File("qr-code.svg"));
+    final QrCodeSvgGenerator qrCodeGenerator = new QrCodeSvgGenerator();
+    final Document logo = parseBase64EncodedStringToDocument("Replace-by-Base64-encoded-SVGImage");
+    final Document qrCodeImage = qrCodeGenerator.generateQrCode(logo);
+    final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    final Transformer transformer = transformerFactory.newTransformer();
+    final DOMSource source = new DOMSource(qrCodeImage);
+    final StreamResult result = new StreamResult(new File("qr-code.svg"));
     transformer.transform(source, result);
   }
 
   public static Document parseBase64EncodedStringToDocument(final String base64String) throws
       IOException, ParserConfigurationException, SAXException {
-    final var xmlString = Base64.getDecoder().decode(base64String);
-    final var factory = DocumentBuilderFactory.newInstance();
-    final var builder = factory.newDocumentBuilder();
-    final var inputSource = new InputSource(new ByteArrayInputStream(xmlString));
+    final byte[] xmlString = Base64.getDecoder().decode(base64String);
+    final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    final DocumentBuilder builder = factory.newDocumentBuilder();
+    final InputSource inputSource = new InputSource(new ByteArrayInputStream(xmlString));
     return builder.parse(inputSource);
   }
 
   public Document generateQrCode(Document svgLogo) {
-    final var qrPositionalSquaresConfig = new QrPositionalSquaresConfig.Builder()
+    final QrPositionalSquaresConfig qrPositionalSquaresConfig = new QrPositionalSquaresConfig.Builder()
             .circleShaped(true)
             .relativeSquareBorderRound(0.05)
             .centerColor(new Color(255, 0, 0))
@@ -53,7 +55,7 @@ public final class QrCodeSvgGenerator {
             .outerSquareColor(new Color(0, 0, 0))
             .outerBorderColor(new Color(255, 255, 255))
             .build();
-    final var qrCodeConfig = new QrCodeSvgConfig.Builder("https://simonscholz.dev/}")
+    final QrCodeSvgConfig qrCodeConfig = new QrCodeSvgConfig.Builder("https://simonscholz.dev/}")
             .qrCodeSize(800)
             .qrCodeColorConfig(new Color(255, 255, 255), new Color(0, 0, 0))
             .qrLogoConfig(svgLogo, 0.2, new Color(255, 255, 255), LogoShape.CIRCLE)
@@ -65,10 +67,10 @@ public final class QrCodeSvgGenerator {
   }
 
   public byte[] createQrCodeByteArray(Document svgLogo) throws IOException, TransformerException {
-    final var qrCodeGenerator = new QrCodeSvgGenerator();
-    final var qrCodeImage = qrCodeGenerator.generateQrCode(svgLogo);
-    final var byteArrayOutputStream = new ByteArrayOutputStream();
-    final var transformer = TransformerFactory.newInstance().newTransformer();
+    final QrCodeSvgGenerator qrCodeGenerator = new QrCodeSvgGenerator();
+    final Document qrCodeImage = qrCodeGenerator.generateQrCode(svgLogo);
+    final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    final Transformer transformer = TransformerFactory.newInstance().newTransformer();
     transformer.transform(new DOMSource(qrCodeImage), new StreamResult(byteArrayOutputStream));
     return byteArrayOutputStream.toByteArray();
   }
