9334f88036425a9f10a6c152826d35200667556b
Simon Scholz <opensource.simon@gmail.com>
2026-05-15 00:32:21 +0200

    Adjust junit version and change snapshot version

--- java-sample/build.gradle.kts
+++ java-sample/build.gradle.kts
@@ -7,15 +7,18 @@ group = "com.github.simonscholz"
 version = "0.1.0"
 
 repositories {
+    maven {
+        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
+        mavenContent {
+            snapshotsOnly()
+        }
+    }
     mavenCentral()
-    maven(
-        url = "https://central.sonatype.com/repository/maven-snapshots/",
-    )
 }
 
 dependencies {
-    implementation("io.github.simonscholz:qr-code-with-logo:0.5.0-SNAPSHOT")
-    implementation("io.github.simonscholz:qr-code-with-logo-svg:0.5.0-SNAPSHOT")
+    implementation("io.github.simonscholz:qr-code-with-logo:0.6.0-SNAPSHOT")
+    implementation("io.github.simonscholz:qr-code-with-logo-svg:0.6.0-SNAPSHOT")
 
     testImplementation(libs.junit.jupiter.engine)
     testImplementation(libs.junit.jupiter.params)
