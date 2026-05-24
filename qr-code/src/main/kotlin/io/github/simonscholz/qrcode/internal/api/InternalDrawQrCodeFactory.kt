01cc991d21cb2fb203cc64e57abd7297701ebdda
Simon Scholz <opensource.simon@gmail.com>
2026-05-24 23:52:15 +0200

    Fix DOKKA issues

--- qr-code/src/main/kotlin/io/github/simonscholz/qrcode/internal/api/InternalDrawQrCodeFactory.kt
+++ qr-code/src/main/kotlin/io/github/simonscholz/qrcode/internal/api/InternalDrawQrCodeFactory.kt
@@ -2,7 +2,7 @@ package io.github.simonscholz.qrcode.internal.api
 
 /**
  * Used to avoid exposing the implementation of the complete [QrCodeApiImpl] to clients.
- * Clients should use the [QrCodeApi] interface and [QrCodeFactory] to interact with the library.
+ * Clients should use the [io.github.simonscholz.qrcode.QrCodeApi] interface and [io.github.simonscholz.qrcode.QrCodeFactory] to interact with the library.
  */
 object InternalDrawQrCodeFactory {
     fun createInternalDrawQrCode(): InternalDrawQrCode = QrCodeApiImpl()
